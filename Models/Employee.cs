3468cfd9bb52a4af70d04bf9f4846c59f1ab0107
MichaelLem <michaelthomaslem@gmail.com>
2026-04-08 23:10:29 -0700

    Removed unnecessary list

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -3,7 +3,6 @@
     public class Employee
     {
         public int EmployeeId { get; set; }
-        public List<TimeEntry> TimeEntries { get; set; } = new();
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
